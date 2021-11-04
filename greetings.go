85482a205a5ae41cf7fefc0cd96a6795e520b891
Bianca Marcucci <marcucci.bianca96@gmail.com>
2021-11-04 16:59:37 +0000

    package scope

---
+++ greetings.go
@@ -0,0 +1,17 @@
+//For Tutorial 11 on Package Scope (could have done with any other file, as we all put them in package main)//
+// we will define variables and functions we will then use in tutorial11.go and will also use a variable from tutorial11.go
+
+package main
+
+import "fmt"
+
+var points = []int{20, 90, 100, 45, 70}
+
+func sayHello(myString string) {
+	fmt.Println("hello", myString)
+}
+
+// we use variable we declared in tutorial11.go and then we will use showScore() in tutorial11.go
+func showScore() {
+	fmt.Println("You scored this many points: ", score)
+}
---
+++ tutorial11.go
@@ -0,0 +1,23 @@
+//Package Scope//
+// How to use variables and functions from different files from the same package//
+//could have used any file here, but let's use greetings.go, we will use variables and functions from it,
+// viceversa we will also define a variable (at root level, outside the main() function) in tutorial11.go and we will use it in greetings.go
+
+package main
+
+import "fmt"
+
+// we can declare a variable outside the main function to make it visible to greetings.go
+var score = 99.5
+
+func main() {
+	//We use sayHello() and we loop through slice 'points' from greetings.go
+	sayHello("Vision")
+
+	for _, point := range points {
+		fmt.Println(point)
+	}
+	// using another function from greetings.go
+	showScore()
+
+}
