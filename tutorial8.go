f9309135ba38ca5a4332a581900f58e862b2cd44
Bianca Marcucci <marcucci.bianca96@gmail.com>
2021-11-04 14:20:24 +0000

    booleans and conditionals

---
+++ tutorial8.go
@@ -0,0 +1,22 @@
+// Booleans & Conditionals //
+package main
+
+import "fmt"
+
+func main() {
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
+
+	for index, value := range names {
+		// continue is to keep looping
+		if index == 1 {
+			fmt.Println("Continuing at position ", index)
+			continue
+		}
+		// break to break completely from looping
+		if index > 2 {
+			fmt.Println("Breaking the loop at position ", index)
+			break
+		}
+		fmt.Printf("The value at position %v is %v \n", index, value)
+	}
+}
